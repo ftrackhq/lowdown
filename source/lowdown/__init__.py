711c3d024d1e0873643abd8e041115a203e3ddad
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-02-12 12:06:45 +0100

    enforce binary on file_object

--- source/lowdown/__init__.py
+++ source/lowdown/__init__.py
@@ -278,8 +278,8 @@ def copy_stylesheet(app, exception):
 
     source = pkgutil.get_data('lowdown', 'lowdown.css')
     target = os.path.join(app.builder.outdir, '_static', 'lowdown.css')
-    with open(target, 'w') as file_object:
-        file_object.write(str(source))
+    with open(target, 'wb') as file_object:
+        file_object.write(source)
 
 
 def setup(app):
