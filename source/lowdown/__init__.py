7196425be6bc24fcd2a5ce6c93df79ca85ecd856
Lorenzo Angeli <lorenzo.angeli@ftrack.com>
2020-02-12 11:41:37 +0100

    ensure cast to str

--- source/lowdown/__init__.py
+++ source/lowdown/__init__.py
@@ -279,7 +279,7 @@ def copy_stylesheet(app, exception):
     source = pkgutil.get_data('lowdown', 'lowdown.css')
     target = os.path.join(app.builder.outdir, '_static', 'lowdown.css')
     with open(target, 'w') as file_object:
-        file_object.write(source.encode(encoding='UTF-8'))
+        file_object.write(str(source))
 
 
 def setup(app):
